54790b4adb6e5ad52950b48b8ee833531f927fb6
SydVic <fabrizio.bocca27@gmail.com>
2022-05-23 17:59:59 +0200

    aggiunto fallback immagine bandiera quando non presente

--- src/App.vue
+++ src/App.vue
@@ -16,42 +16,39 @@
     <!-- /WRAPPER DELLA RICERCA -->
 
     <!-- WRAPPER DEI RISULTATI DELLA RICERCA FILM -->
-    <div 
-    class="movies-search-result-wrapper"
-    v-for="(item) in filmResults" :key="item.id">
-      <img class="poster" :src="`https://image.tmdb.org/t/p/w342${item.poster_path}`" :alt="item.original_title">
-      <h2 class="title">{{ item.title }}</h2>
-      <h3 class="original-title">{{ item.original_title }}</h3>
-      <div class="language">
-        <!-- <img 
-        v-if="(`./assets/flags/${item.original_language}.png`)"
-        class="flag"
-        :src="require(`./assets/flags/${item.original_language}.png`)" 
-        :alt="item.original_language">
-        <h4 v-else>{{item.original_language}}</h4> -->
-        <h4>{{item.original_language}}</h4>
+    <div class="movies-search-result-wrapper">
+      <div class="film-card" v-for="(item) in filmResults" :key="item.id">
+        <img class="poster" :src="`https://image.tmdb.org/t/p/w300${item.poster_path}`" :alt="item.original_title">
+        <h2 class="title">{{ item.title }}</h2>
+        <h3 class="original-title">{{ item.original_title }}</h3>
+        <div class="language">
+          <img 
+          class="flag"
+          :src="getFlag(item)" 
+          :alt="item.original_language">
+         
+          <!-- <h4>{{item.original_language}}</h4> -->
+        </div>
+        <h5 class="rating">{{ ratingConversion(item.vote_average) }}</h5>
       </div>
-      <h5 class="rating">{{ item.vote_average }}</h5>
     </div>
     <!-- /WRAPPER DEI RISULTATI DELLA RICERCA FILM -->
 
     <!-- WRAPPER DEI RISULTATI DELLA RICERCA SERIE TV -->
     <div 
-    class="tvseries-search-result-wrapper"
-    v-for="(item) in tvSeriesResults" :key="item.id">
-      <img class="poster" :src="`https://image.tmdb.org/t/p/w342${item.poster_path}`" :alt="item.original_name">
-      <h2 class="title">{{ item.name }}</h2>
-      <h3 class="original-title">{{ item.original_name }}</h3>
-      <div class="language">
-        <!-- <img 
-        v-if="(`./assets/flags/${item.original_language}.png`)"
-        class="flag"
-        :src="require(`./assets/flags/${item.original_language}.png`)" 
-        :alt="item.original_language">
-        <h4 v-else>{{item.original_language}}</h4> -->
-        <h4>{{item.original_language}}</h4>
+    class="tvseries-search-result-wrapper">
+      <div class="tv-series-card" v-for="(item) in tvSeriesResults" :key="item.id">
+        <img class="poster" :src="`https://image.tmdb.org/t/p/w300${item.poster_path}`" :alt="item.original_name">
+        <h2 class="title">{{ item.name }}</h2>
+        <h3 class="original-title">{{ item.original_name }}</h3>
+        <div class="language">
+          <img 
+          class="flag"
+          :src="getFlag(item)" 
+          :alt="item.original_language">
+        </div>
+        <h5 class="rating">{{ ratingConversion(item.vote_average) }}</h5>
       </div>
-      <h5 class="rating">{{ item.vote_average }}</h5>
     </div>
     <!-- /WRAPPER DEI RISULTATI DELLA RICERCA SERIE TV -->
   </div>
@@ -71,10 +68,17 @@ export default {
       searchInput: "",
       filmResults: [],
       tvSeriesResults: [],
-      // searchResults: []
     }
   },
   methods: {
+    getFlag(item) {
+      const lang = item.original_language;
+      if (['it', 'en', 'de', 'es', 'fr'].includes(lang)) {
+        return require(`./assets/flags/${item.original_language}.png`)
+      } else {
+        return item.original_language;
+      }
+    },
     startSearch() {
       axios.get('https://api.themoviedb.org/3/search/movie', {
         params: {
@@ -93,14 +97,25 @@ export default {
       })
       .then((resp) => {
         this.tvSeriesResults = resp.data.results;
-        // this.searchResults = [...this.filmResults, ...this.tvSeriesResults];
       });      
     },
+    ratingConversion(vote) {
+      const convertedNumber = Math.round(vote / 2);
+      return convertedNumber;
+    }
   }
 }
 </script>
 
 <style lang="scss">
+.movies-search-result-wrapper {
+  display: flex;
+  flex-wrap: wrap;
+}
+.tvseries-search-result-wrapper {
+  display: flex;
+  flex-wrap: wrap;
+}
 .flag {
   width:20px;
   height: 20px;
