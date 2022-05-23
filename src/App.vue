be59f579993339a0f1a1af7f559d29030b7bd4c1
SydVic <fabrizio.bocca27@gmail.com>
2022-05-23 12:14:50 +0200

    aggiunto input e intercettato con v-model in data

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,34 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="search-wrapper">
+      <input 
+      class="search-input" 
+      type="text" 
+      placeholder="titolo film o parola chiave"
+      v-model="searchInput">
+
+      <button 
+      class="search-btn"
+      @keyup.enter="startSearch">Avvia ricerca</button>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+
+  },
+  data: function() {
+    return {
+      searchInput: "",
+    }
   }
 }
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
