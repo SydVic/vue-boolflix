fbe96aaf604237573dc9e3ce58c4567d10185702
SydVic <fabrizio.bocca27@gmail.com>
2022-05-24 12:54:38 +0200

    aggiunto backdrop_path se poster_path non presente e jpg se assenti entrambi

--- src/components/AppCard.vue
+++ src/components/AppCard.vue
@@ -1,7 +1,8 @@
 <template>
   <div class="card">
-    <img class="poster" :src="`https://image.tmdb.org/t/p/w300${movie.poster_path}`" :alt="getOriginalTitle(movie)">
-    <!-- <h2 class="title">{{ movie.title }}</h2> -->
+    <!-- <img class="poster" :src="`https://image.tmdb.org/t/p/w300${movie.poster_path}`" :alt="getOriginalTitle(movie)"> -->
+    <!-- <img class="poster" :src="`https://image.tmdb.org/t/p/w342${movie.backdrop_path}`" :alt="getOriginalTitle(movie)"> -->
+    <img class="poster" :src="getPosterImg(movie)" :alt="getOriginalTitle(movie)">
     <h2 class="title">{{ getTitle(movie) }}</h2>
     <h3 class="original-title">{{ getOriginalTitle(movie) }}</h3>
     <div class="language">
@@ -9,15 +10,15 @@
       class="flag"
       :src="getFlag(movie)" 
       :alt="movie.original_language">
-      </div>
-      <h5 class="rating">{{ convertRating(movie.vote_average) }}
-        <div v-for="n in convertRating(movie.vote_average)" :key="n" class="stars">
+    </div>
+      <!-- <h5 class="rating">{{ convertRating(movie.vote_average) }} -->
+        <!-- <span v-for="n in convertRating(movie.vote_average)" :key="n" class="stars-full">
           <i class="fas fa-star"></i>
-        </div>
-        <div v-for="n in (5 - convertRating(movie.vote_average))" :key="n" class="stars">
+        </span>
+        <span v-for="n in (5 - convertRating(movie.vote_average))" :key="n" class="stars-empty">
           <i class="far fa-star"></i>
-        </div>
-      </h5>
+        </span> -->
+      <!-- </h5> -->
   </div>
 </template>
 
@@ -28,6 +29,16 @@ export default {
     movie: Object,
   },
   methods: {
+    getPosterImg(movie) {
+      if (movie.poster_path) {
+        return `https://image.tmdb.org/t/p/w342${movie.poster_path}`;
+      } else if (movie.backdrop_path)
+      {
+        return `https://image.tmdb.org/t/p/w342${movie.backdrop_path}`;
+      } else {
+        return require('../assets/img/poster-holder.jpg');
+      }
+    },
     getFlag(movie) {
       const lang = movie.original_language;
       if (['it', 'en', 'de', 'es', 'fr'].includes(lang)) {
@@ -68,10 +79,14 @@ export default {
   text-align: center;
   margin: 10px;
   padding: 10px;
-  width: calc(100% / 5 - 20px);
+  width: calc(100% / 4 - 20px);
 
   img {
     width: 100%;
+  }
+
+  .stars-full {
+    color: #ffd700;
   }
 
   .flag {
